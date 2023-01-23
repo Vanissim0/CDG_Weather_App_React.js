d95990a4f8bcf117a4ad2152b3dd4e10de54d278
IVaN Ivanovi4 <eng.vano@yandex.ru>
2023-01-23 23:38:38 +0300

    Add 'final' static

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import TopButton from './components/TopButton';
 import Input from './components/Input';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
     shadow-gray-400">
       <TopButton/>
+
       <Input/>
+
       <TimeAndLocation />
+
       <TemperatureAndDetails />
+
+      <Forecast title='hourly forecast' />
+      <Forecast title='daily forecast' />
+
     </div>
   );
 }
---
+++ src/components/Forecast.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+export default function Forecast({title}) {
+  return (
+    <div>
+        <div className='flex items-center justify-start mt-6'> 
+            <p className='text-white font-medium uppercase'>
+                {title}
+            </p>
+        </div>
+
+        <hr className='my-2' />
+
+        <div className='flex flex-row items-center justify-between
+        text-white'
+        >
+
+            <div className='flex flex-col items-center
+            justify-center'>
+                <p className='font-light text-sm'>
+                    04:30 PM
+                </p>
+                <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+                    alt="" 
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22*</p>
+            </div>
+
+            <div className='flex flex-col items-center
+            justify-center'>
+                <p className='font-light text-sm'>
+                    04:30 PM
+                </p>
+                <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+                    alt="" 
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22*</p>
+            </div>
+
+            <div className='flex flex-col items-center
+            justify-center'>
+                <p className='font-light text-sm'>
+                    04:30 PM
+                </p>
+                <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+                    alt="" 
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22*</p>
+            </div>
+
+            <div className='flex flex-col items-center
+            justify-center'>
+                <p className='font-light text-sm'>
+                    04:30 PM
+                </p>
+                <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+                    alt="" 
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22*</p>
+            </div>
+
+            <div className='flex flex-col items-center
+            justify-center'>
+                <p className='font-light text-sm'>
+                    04:30 PM
+                </p>
+                <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+                    alt="" 
+                    className='w-12 my-1'
+                />
+                <p className='font-medium'>22*</p>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
